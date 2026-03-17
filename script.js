ab589b7ab3fd1f12b1432dab78cea1dd99d7aaaf
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-17 21:34:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+// focus first input
+codes[0].focus();
+
+codes.forEach((code, index) => {
+
+  code.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    // allow only numbers
+    if (!/^[0-9]$/.test(value)) {
+      code.value = "";
+      return;
+    }
+
+    // move to next input
+    if (value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+
+    if (e.key === "Backspace") {
+      // if empty → go back
+      if (!code.value && index > 0) {
+        codes[index - 1].focus();
+      }
+    }
+  });
+
+});
